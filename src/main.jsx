6ec5acdaedf4454c9d58f2553ec1d88e548b98b3
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-01-18 01:10:56 +0600

    implement react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import router from './routes/router'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
