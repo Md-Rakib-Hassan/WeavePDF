2f4d62bf549d89b6c090ddd43a36d91a0e867df5
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-01-26 15:32:11 +0600

    added a modal with input field

--- src/Pages/HtmlToPDF/HtmlToPDF.jsx
+++ src/Pages/HtmlToPDF/HtmlToPDF.jsx
@@ -7,7 +7,31 @@ const HtmlToPDF = () => {
                 <div className="max-w-lg">
                     <h1 className="text-5xl font-bold">HTML to PDF</h1>
                     <p className="py-4">Transform web pages into PDF files with precision and reliability.</p>
-                    <button className="buttonProject3">Add HTML to get Started </button>
+                    <label htmlFor="my_modal_6" className="buttonProject3">Add HTML to get Started </label>
+                </div>
+            </div>
+
+
+            {/* The button to open modal */}
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+            <div className="modal" role="dialog">
+                <div className="modal-box">
+                    <div className="modal-action">
+                        <label htmlFor="my_modal_6" className="btn font-bold ">X</label>
+                    </div>
+                    <h1 className="text-center text-2xl font-bold">Add HTML to convert from</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <span className="label-text my-3 font-bold">Write the website URL</span>
+                            <input type="text" placeholder="Website Url" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-3">
+                            <button className="buttonProject3 w-2/5 mx-auto">Convert To PDF</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
