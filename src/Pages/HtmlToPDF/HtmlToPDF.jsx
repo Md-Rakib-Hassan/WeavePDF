becb27ad32700f96d1d1e417b4b1c87e6b1dcf16
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-01-26 15:12:45 +0600

    Created html to pdf converter page

--- src/Pages/HtmlToPDF/HtmlToPDF.jsx
+++ src/Pages/HtmlToPDF/HtmlToPDF.jsx
@@ -2,8 +2,14 @@
 
 const HtmlToPDF = () => {
     return (
-        <div>
-
+        <div className="hero min-h-[70vh] bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-lg">
+                    <h1 className="text-5xl font-bold">HTML to PDF</h1>
+                    <p className="py-4">Transform web pages into PDF files with precision and reliability.</p>
+                    <button className="buttonProject3">Add HTML to get Started </button>
+                </div>
+            </div>
         </div>
     );
 };
