90d4d7d64895118d14bf3701479bd0823f28781a
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-01-26 14:54:51 +0600

    Route created for html to pdf feature

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,40 +1,54 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Root from "../layout/Root";
 import Home from "../pages/Home";
 import Subscriptions from "../pages/Subscriptions";
 import Contact from './../pages/Contact';
 import MergePdf from "../Pages/MergePdf/MergePdf";
+import DrawSignature from "../Pages/DrawSignature/DrawSignature";
+import AddSignature from "../Pages/AddSignature/AddSignature";
+import HtmlToPDF from "../Pages/HtmlToPDF/HtmlToPDF";
 
 
-  const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Root></Root>,
-        children: [
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
 
-            {
-              path: '/',
-              element: <Home></Home>
-            },
+      {
+        path: '/',
+        element: <Home></Home>
+      },
 
-            {
-              path:'subscriptions',
-              element:<Subscriptions></Subscriptions>
-            },
-            {
-              path:'/contact',
-              element:<Contact></Contact>,
-            },
-            {
-              path: '/merge-pdf',
-              element: <MergePdf></MergePdf>
-            }
-        
-          ]
-        }
+      {
+        path: 'subscriptions',
+        element: <Subscriptions></Subscriptions>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>,
+      },
+      {
+        path: '/merge-pdf',
+        element: <MergePdf></MergePdf>
+      },
+      {
+        path: '/htmlToPdf',
+        element: <HtmlToPDF></HtmlToPDF>
+      },
+      {
+        path: '/draw-signature',
+        element: <DrawSignature></DrawSignature>
+      },
+      {
+        path: '/add-signature',
+        element: <AddSignature></AddSignature>
+      }
+    ]
+  }
 
-  ]);
+]);
 
-  export default router;
+export default router;
