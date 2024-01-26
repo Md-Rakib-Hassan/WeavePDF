564240a74e52079d2e7ad55cb75f66d0e97f05de
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-01-26 15:57:18 +0600

    added a input file for select a html file for contert to pdf

--- src/Pages/HtmlToPDF/HtmlToPDF.jsx
+++ src/Pages/HtmlToPDF/HtmlToPDF.jsx
@@ -6,7 +6,7 @@ const HtmlToPDF = () => {
             <div className="hero-content text-center">
                 <div className="max-w-lg">
                     <h1 className="text-5xl font-bold">HTML to PDF</h1>
-                    <p className="py-4">Transform web pages into PDF files with precision and reliability.</p>
+                    <p className="py-6">Transform web pages into PDF files with precision and reliability.</p>
                     <label htmlFor="my_modal_6" className="buttonProject3">Add HTML to get Started </label>
                 </div>
             </div>
@@ -19,13 +19,22 @@ const HtmlToPDF = () => {
             <div className="modal" role="dialog">
                 <div className="modal-box">
                     <div className="modal-action">
-                        <label htmlFor="my_modal_6" className="btn font-bold ">X</label>
+                        <label htmlFor="my_modal_6" className="btn font-bold rounded-full">X</label>
                     </div>
                     <h1 className="text-center text-2xl font-bold">Add HTML to convert from</h1>
-                    <form className="card-body">
+                    <form className="card-body border-2 mt-3">
                         <div className="form-control">
                             <span className="label-text my-3 font-bold">Write the website URL</span>
                             <input type="text" placeholder="Website Url" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-3">
+                            <button className="buttonProject3 w-2/5 mx-auto">Convert To PDF</button>
+                        </div>
+                    </form>
+                    <form className="card-body border-2 mt-2">
+                        <div className="form-control">
+                            <span className="label-text my-3 font-bold">Select Your HTML FIle</span>
+                            <input type="file" className="file-input w-full max-w-xs" />
                         </div>
                         <div className="form-control mt-3">
                             <button className="buttonProject3 w-2/5 mx-auto">Convert To PDF</button>
