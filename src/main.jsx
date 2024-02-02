267e7c8c4e7ea273c2e0220596a2fd4d76c78772
Maliha <maliha16tabassum@gmail.com>
2024-02-02 10:12:41 +0600

    added strict mode for prod

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
+  <React.StrictMode>
     <AuthProviders>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProviders>
-  
+    </React.StrictMode>
 );
 
