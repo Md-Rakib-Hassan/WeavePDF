492ab759a04ac71aa3a2b5e36c9ba92e2f9fb1b5
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-02-06 22:02:47 +0600

    Complete the download feacher

--- src/Pages/MdToPdf/Editor.jsx
+++ src/Pages/MdToPdf/Editor.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
 import rehypeKatex from 'rehype-katex'
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { vs } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
+import jsPDF from 'jspdf';
 const Editor = () => {
 
     const [input,setInput]=useState();
+    const generatePDF=()=>{
+      const doc= new jsPDF("p","pt","a4");
+      doc.html(document.querySelector("#prose"),{
+        callback: function(pdf){
+          pdf.save("mypdf.pdf");
+        }
+      })
+
+    }
     return (
         <div className='flex' >
             <textarea autoFocus className=' min-h-[100vh] w-1/2 p-4 border bg-neutral-50' value={input} onChange={(e)=>setInput(e.target.value)}></textarea>
-            <div className='w-1/2 p-4'>
+            <div className='w-1/2 p-4 text-black' id='prose'>
                 <Markdown 
                 remarkPlugins={remarkGfm}
                 rehypePlugins={[rehypeKatex]}
@@ -38,8 +47,9 @@ const Editor = () => {
                   }}
                 
                 ></Markdown>
+                
             </div>
-            
+            <button onClick={generatePDF}>Download Pdf</button>
         </div>
     );
 };
