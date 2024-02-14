5c9d25acaf59d73aadeb950977d1e7793e32dd78
Maliha <maliha16tabassum@gmail.com>
2024-02-14 16:01:53 +0600

    ui enriched in edit pdf

--- src/Pages/EditPDF/EditPdf.jsx
+++ src/Pages/EditPDF/EditPdf.jsx
@@ -32,10 +32,21 @@ const EditPdf = () => {
     }
     return (
         <div className="MyComponent h-full">
-          <input type="file" accept='pdf' onChange={handleChange} name="" id="" />
-          { doce &&
+          {!doce ?
           <div>
-            <div className="header">React sample</div>
+            <br />
+             <h1 className='text-3xl font-playfair font-bold text-center'>Edit your PDF Files here</h1><br />
+            <p className='text-center'>Edit PDFs in the order you want with the easiest PDF editor available.</p><br />
+            <div className='flex justify-center'>
+                <label className='label w-48'>
+                <input type="file" accept='application/pdf' onChange={handleChange} name="" id="merge-input" />
+                <span className='font-bold text-xl text-white'>Select PDF file</span>
+                </label>
+            </div>
+            
+            </div>
+            :
+          <div>
           <div className="webviewer h-[600px]" ref={viewer}></div>
           </div>
           } 
