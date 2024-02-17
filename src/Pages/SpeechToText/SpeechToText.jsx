e43d0b438889368e3369afcc2cb593bd83c2f843
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-02-17 13:47:29 +0600

    Feature implementation done

--- src/Pages/SpeechToText/SpeechToText.jsx
+++ src/Pages/SpeechToText/SpeechToText.jsx
@@ -2,6 +2,9 @@ import 'regenerator-runtime/runtime'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from "react-use-clipboard";
 import { useState } from "react";
+import { IoIosCopy } from "react-icons/io";
+import { CiPause1 } from "react-icons/ci";
+import { MdOutlineSettingsVoice } from "react-icons/md";
 import "./SpeechToText.css"
 
 const SpeechToText = () => {
@@ -9,8 +12,6 @@ const SpeechToText = () => {
     const [isCopied, setCopied] = useClipboard(textToCopy, {
         successDuration: 1000
     });
-
-    //subscribe to thapa technical for more awesome videos
 
     const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
     const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
@@ -21,20 +22,20 @@ const SpeechToText = () => {
 
     return (
 
-        <div className='flex h-screen justify-center items-center'>
+        <div className='lg:flex lg:h-screen justify-center items-center'>
             <div className="container mx-auto">
-                <h2 className='lg:text-4xl font-bold text-center'>Speech to Text Converter</h2>
+                <h2 className='lg:text-4xl text-2xl font-bold text-center'>Speech to Text Converter</h2>
                 <br />
 
                 <div className="main-content" onClick={() => setTextToCopy(transcript)}>
                     {transcript}
                 </div>
-                <div className="lg:flex justify-center lg:max-w-[50rem] mx-auto">
+                <div className="lg:flex justify-between lg:max-w-[57rem] mx-auto">
                     <button className='button-class' onClick={setCopied}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
+                        {isCopied ? 'Copied!' : <><span className='flex gap-1 items-center'><IoIosCopy></IoIosCopy> Copy to clipboard</span></>}
                     </button>
-                    <button className='button-class' onClick={startListening}>Start Listening</button>
-                    <button className='button-class' onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+                    <button className='button-class button-class flex gap-1 items-center' onClick={startListening}><MdOutlineSettingsVoice className='text-lg'></MdOutlineSettingsVoice>Start Listening</button>
+                    <button className='button-class flex gap-1 items-center' onClick={SpeechRecognition.stopListening}><CiPause1></CiPause1> Stop Listening</button>
                 </div>
             </div>
 
