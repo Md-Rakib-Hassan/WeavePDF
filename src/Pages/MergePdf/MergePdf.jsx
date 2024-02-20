bc44b4f3809c63122554d5a1ca238718648c731d
Maliha <maliha16tabassum@gmail.com>
2024-02-20 19:41:19 +0600

    [bugfix]: empty req body

--- src/Pages/MergePdf/MergePdf.jsx
+++ src/Pages/MergePdf/MergePdf.jsx
@@ -25,7 +25,7 @@ const MergePdf = () => {
 
         const service = {  date, user_email, no_of_files, service_name, status, file}
         console.log(service);
-        axiosPublic.post('/upload-service')
+        axiosPublic.post('/upload-service',service)
         .then(res=>console.log(res.data));
         // setIsOn(true)
         
