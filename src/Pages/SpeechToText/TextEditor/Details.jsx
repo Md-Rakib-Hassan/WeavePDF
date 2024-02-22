df751a7f56baee522a88050ca933bec45317f51b
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-02-23 02:45:19 +0600

    A another new package installed named @tiptap/starter-kit

---
+++ src/Pages/SpeechToText/TextEditor/Details.jsx
@@ -0,0 +1,11 @@
+
+
+const Details = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Details;
--- src/Pages/SpeechToText/TextEditor/TextEditor.jsx
+++ src/Pages/SpeechToText/TextEditor/TextEditor.jsx
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
+import Tiptap from "./Tiptap";
+import Details from "./Details";
 
 const TextEditor = () => {
-    return (
-        <div>
+    const [description, setDescription] = useState("");
 
+    return (
+        <div className="App">
+            <Tiptap setDescription={setDescription} />
+            <Details description={description} />
         </div>
     );
-};
+}
 
 export default TextEditor;
