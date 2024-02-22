505555ddafccd65771a5cbd87bc953babf0e2847
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-02-23 02:51:38 +0600

    Another package installed named react icon

--- src/Pages/SpeechToText/SpeechToText.jsx
+++ src/Pages/SpeechToText/SpeechToText.jsx
@@ -6,6 +6,7 @@ import { IoIosCopy } from "react-icons/io";
 import { CiPause1 } from "react-icons/ci";
 import { MdOutlineSettingsVoice } from "react-icons/md";
 import "./SpeechToText.css"
+import TextEditor from './TextEditor/TextEditor';
 
 const SpeechToText = () => {
     const [textToCopy, setTextToCopy] = useState();
@@ -22,23 +23,27 @@ const SpeechToText = () => {
 
     return (
 
-        <div className='lg:flex lg:h-screen justify-center items-center'>
-            <div className="container mx-auto">
-                <h2 className='lg:text-4xl text-2xl font-bold text-center'>Speech to Text Converter</h2>
-                <br />
+        <div>
+            <div className='lg:flex lg:h-screen justify-center items-center'>
+                <div className="container mx-auto">
+                    <h2 className='lg:text-4xl text-2xl font-bold text-center'>Speech to Text Converter</h2>
+                    <br />
 
-                <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-                    {transcript}
+                    <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+                        {transcript}
+                    </div>
+                    <div className="lg:flex justify-between lg:max-w-[57rem] mx-auto">
+                        <button className='button-class' onClick={setCopied}>
+                            {isCopied ? 'Copied!' : <><span className='flex gap-1 items-center'><IoIosCopy></IoIosCopy> Copy to clipboard</span></>}
+                        </button>
+                        <button className='button-class button-class flex gap-1 items-center' onClick={startListening}><MdOutlineSettingsVoice className='text-lg'></MdOutlineSettingsVoice>Start Listening</button>
+                        <button className='button-class flex gap-1 items-center' onClick={SpeechRecognition.stopListening}><CiPause1></CiPause1> Stop Listening</button>
+                    </div>
                 </div>
-                <div className="lg:flex justify-between lg:max-w-[57rem] mx-auto">
-                    <button className='button-class' onClick={setCopied}>
-                        {isCopied ? 'Copied!' : <><span className='flex gap-1 items-center'><IoIosCopy></IoIosCopy> Copy to clipboard</span></>}
-                    </button>
-                    <button className='button-class button-class flex gap-1 items-center' onClick={startListening}><MdOutlineSettingsVoice className='text-lg'></MdOutlineSettingsVoice>Start Listening</button>
-                    <button className='button-class flex gap-1 items-center' onClick={SpeechRecognition.stopListening}><CiPause1></CiPause1> Stop Listening</button>
-                </div>
+
             </div>
 
+            <TextEditor></TextEditor>
         </div>
     );
 }
--- src/Pages/SpeechToText/TextEditor/Details.jsx
+++ src/Pages/SpeechToText/TextEditor/Details.jsx
@@ -1,10 +1,23 @@
+import jsPDF from "jspdf";
+import { useState } from "react";
 
 
-const Details = () => {
+const Details = ({ description }) => {
+    const [isOn, setIsOn] = useState(false);
+    const generatePDF = () => {
+        const doc = new jsPDF("p", "pt", "a4");
+        doc.html(document.querySelector("#detailsButton"), {
+            callback: function (pdf) {
+                pdf.save("md-to-pdf.pdf");
+            }
+        })
+        setIsOn(true);
+
+    }
     return (
-        <div>
-
-        </div>
+        <>
+            <div id='detailsButton' className="ProseMirrorr">{description ? <button className='absolute right-2 bg-blue px-2 rounded-md' onClick={generatePDF}>Download Pdf</button> : ''} {parse(description)}</div>
+        </>
     );
 };
 
