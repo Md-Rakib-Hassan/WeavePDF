b1e75c87efc68e012e6ed196f38918b4ff5b6289
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-24 01:00:21 +0600

    Add props setup todo task

---
+++ src/Pages/ToDoList/ToDoTask.jsx
@@ -0,0 +1,21 @@
+
+
+import { FaTrash } from "react-icons/fa";
+
+function ToDoTask(props) {
+  return (
+    <li className="flex items-center p-4">
+      <div className="flex items-center gap-2">
+        <span className="text-lg">{props.item}</span>
+        <FaTrash
+          className="text-red-500 cursor-pointer hover:text-red-700"
+          onClick={() => {
+            props.deleteItem(props.index);
+          }}
+        />
+      </div>
+    </li>
+  );
+}
+
+export default ToDoTask;
