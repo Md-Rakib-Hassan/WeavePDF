96edc95467e72caec713f577dbff269c0e31a6c6
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-24 01:06:11 +0600

    Implement todo task Input button file

--- src/Pages/ToDoList/ToDoList.jsx
+++ src/Pages/ToDoList/ToDoList.jsx
@@ -19,7 +19,7 @@ function ToDoList() {
     <div className="max-w-7xl mx-auto py-10">
       <div className="flex flex-col items-center pt-10">
         <h1 className="text-3xl font-bold pb-3">Create Your TODO List</h1>
-        <p className="pb-10">Organize Your Day: The Ultimate Todo List Guide</p>
+        <p className="pb-10">Organize Your Day: The Ultimate ToDo List Guide</p>
         <TodoInput addList={addList} />
         <hr />
         {listTodo.map((listItem, i) => {
