882a870c8fd28b3629497c6cfabb303b179a2f1b
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-24 01:06:44 +0600

    Implement todo list Input  file

--- src/Pages/ToDoList/ToDoList.jsx
+++ src/Pages/ToDoList/ToDoList.jsx
@@ -17,7 +17,7 @@ function ToDoList() {
 
   return (
     <div className="max-w-7xl mx-auto py-10">
-      <div className="flex flex-col items-center pt-10">
+      <div className="flex flex-col items-center pt-9">
         <h1 className="text-3xl font-bold pb-3">Create Your TODO List</h1>
         <p className="pb-10">Organize Your Day: The Ultimate ToDo List Guide</p>
         <TodoInput addList={addList} />
