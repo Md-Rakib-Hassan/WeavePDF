0e477734bca4a965c96a9aabd50558cd26c396f6
Maliha <maliha16tabassum@gmail.com>
2024-02-24 22:30:12 +0600

    added handlepdf method

--- src/Pages/AddNumber/AddNumber.jsx
+++ src/Pages/AddNumber/AddNumber.jsx
@@ -3,10 +3,19 @@ import { PDFDocument,rgb } from 'pdf-lib'
 
 const AddNumber = () => {
     const [url, setUrl] = useState(null);
+    const [pdf, setPdf]= useState(null);
+
+    const handlePdf = e =>{
+        e.preventDefault();
+        const file = document.getElementById('merge-input').files[0];
+        setPdf(file)
+    }
+
     const handleUpload = async (e) =>{
         e.preventDefault();
+        setUrl(null);
         const form = e.target;
-        const filedata = document.getElementById('input').files[0];
+        const filedata = pdf;
         const position = form.position.value;
         const color = form.color.value;
         const existingpdfBytes = await filedata.arrayBuffer();
@@ -21,7 +30,7 @@ const AddNumber = () => {
             let bottomX = 0;
 
             if(position === 'left') bottomX = 30
-            else if(position === 'right') bottomX = pageWidth - 70
+            else if(position === 'right') bottomX = pageWidth - 80
             else bottomX = pageWidth/2
 
             if (color === 'black'){
@@ -60,6 +69,7 @@ const AddNumber = () => {
             const pdfBytes = await pdfDoc.save();
             const pdfBlob = new Blob([pdfBytes] , { type: 'application/pdf' })
             setUrl(URL.createObjectURL(pdfBlob))
+            setPdf(false)
         }
         
         
@@ -69,25 +79,37 @@ const AddNumber = () => {
             <br /><br />
             <h1 className='text-3xl font-playfair text-center font-bold'>Add Page number to PDF Files here</h1><br />
             <p className="text-center">Add page numbers into PDFs with ease. Choose your positions, dimensions, typography.</p>
-            <form onSubmit={handleUpload} action="">
-            <input type="file" accept="application/pdf" name="file" id="input" required /><br />
-            <label htmlFor="position">Position for Page Number</label>
-            <select name="position" id="" required>
-                <option value="left">Left</option>
-                <option value="right">Right</option>
-                <option value="middle">Middle</option>
-            </select><br />
-            <select name="color" id="" required>
-                <option value="black">Black</option>
-                <option value="blue">Blue</option>
-                <option value="red">Red</option>
-                <option value="green">Green</option>
-            </select><br />
-            {/* <input type="text" name="font" id="" placeholder="add font"/> */}
-            <button className="btn"><input type="submit" value="Add Number" /></button>
-            </form>
             <div className="flex justify-center">
-            <iframe src={url} title="Page Number Added" width={600} height={700} ></iframe>
+        <form onSubmit={handleUpload} action="">
+        {pdf ? 
+        <div className="flex justify-center">
+            <button className='btn bg-[#52ab98] mt-5 font-bold text-2xl text-white'>
+                <input type="submit" value="Add Number" /><br />
+            </button><br /></div>
+            :<label className='label w-48'>
+                <input onChange={handlePdf} type="file" accept="application/pdf" name="file" id="merge-input" required />
+                <span className='font-bold text-xl text-white'>Select PDF files</span>
+            </label>
+        }
+            <br />
+                    <label className="mt-5" htmlFor="position">Position: </label>
+                    <select className="p-2" name="position" id="" required>
+                        <option value="left">Left</option>
+                        <option value="right">Right</option>
+                        <option value="middle">Middle</option>
+                    </select>
+                    <label className="mx-5" htmlFor="position">Color: </label>
+                    <select className="p-2" name="color" id="" required>
+                        <option value="black">Black</option>
+                        <option value="blue">Blue</option>
+                        <option value="red">Red</option>
+                        <option value="green">Green</option>
+                    </select><br />
+                    </form>
+            </div>
+            <br />
+            <div className="flex justify-center">
+            <iframe src={url} title="Page Number Added" width={1000} height={500} ></iframe>
             </div>
         </div>
     );
