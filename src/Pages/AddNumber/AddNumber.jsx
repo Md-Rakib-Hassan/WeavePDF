3ceaf65a773b88525c65cda54b3a3ab20312c801
Maliha <maliha16tabassum@gmail.com>
2024-02-24 20:21:44 +0600

    added color as input

--- src/Pages/AddNumber/AddNumber.jsx
+++ src/Pages/AddNumber/AddNumber.jsx
@@ -10,26 +10,27 @@ const AddNumber = () => {
         const form = e.target;
         const filedata = document.getElementById('input').files[0];
         const position = form.position.value;
-        
+        const color = form.color.value;
         const existingpdfBytes = await filedata.arrayBuffer();
         const pdfDoc = await PDFDocument.load(existingpdfBytes);
         const totalPages = pdfDoc.getPageCount();
 
         for(let i=0; i<totalPages ; i++){
-            const page = pdfDoc.getPages([i]);
+            const page = pdfDoc.getPages()[i];
             const text = `Page ${i+1} of ${totalPages}`;
-            const pageSize = page.getSize();
+            const pageWidth = page.getSize().width;
             const bottomY = 20;
             let bottomX = 0;
 
             if(position === 'left') bottomX = 30
-            else if(position === 'right') bottomX = pageSize.width - 70
-            else bottomX = pageSize.width/2
+            else if(position === 'right') bottomX = pageWidth - 70
+            else bottomX = pageWidth/2
 
-            page.drawtext(text, {
+            page.drawText(text, {
                 x: bottomX,
                 y: bottomY,
-                size: 12
+                size: 12,
+                color: color
             })
             const pdfBytes = await pdfDoc.save();
             const pdfBlob = new Blob([pdfBytes] , { type: 'application/pdf' })
@@ -48,8 +49,12 @@ const AddNumber = () => {
                 <option value="right">Right</option>
                 <option value="middle">Middle</option>
             </select><br />
-            <input type="submit" value="Add Number" />
+            <input type="text" name="color" id="" placeholder="colorname or rgb or hex"/>
+            <button className="btn"><input type="submit" value="Add Number" /></button>
             </form>
+            <div className="flex justify-center">
+            <iframe src={url} title="Page Number Added" width={600} height={700} ></iframe>
+            </div>
         </div>
     );
 };
