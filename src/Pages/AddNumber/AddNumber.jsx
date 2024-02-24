d6e36e435c563e574f9ecd7ce580bf89f454737c
Maliha <maliha16tabassum@gmail.com>
2024-02-24 19:48:01 +0600

    added draw text method on each page

--- src/Pages/AddNumber/AddNumber.jsx
+++ src/Pages/AddNumber/AddNumber.jsx
@@ -1,22 +1,44 @@
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { PDFDocument } from 'pdf-lib'
 
 const AddNumber = () => {
     const axiosPublic = useAxiosPublic();
-    const handleUpload = (e) =>{
+    const handleUpload = async (e) =>{
         e.preventDefault();
         const form = e.target;
         const filedata = document.getElementById('input').files[0];
         const position = form.position.value;
         
+        const existingpdfBytes = await filedata.arrayBuffer();
+        const pdfDoc = await PDFDocument.load(existingpdfBytes);
+        const totalPages = pdfDoc.getPageCount();
 
+        for(let i=0; i<totalPages ; i++){
+            const page = pdfDoc.getPages([i]);
+            const text = `Page ${i+1} of ${totalPages}`;
+            const pageSize = page.getSize();
+            const bottomY = 20;
+            let bottomX = 0;
+
+            if(position === 'left') bottomX = 30
+            else if(position === 'right') bottomX = pageSize.width - 70
+            else bottomX = pageSize.width/2
+
+            page.drawtext(text, {
+                x: bottomX,
+                y: bottomY,
+                size: 12
+            })
+        }
+        
         
     }
     return (
         <div>
             <form onSubmit={handleUpload} action="">
-            <input type="file" accept="application/pdf" name="file" id="input" /><br />
+            <input type="file" accept="application/pdf" name="file" id="input" required /><br />
             <label htmlFor="position">Position for Page Number</label>
-            <select name="position" id="">
+            <select name="position" id="" required>
                 <option value="left">Left</option>
                 <option value="right">Right</option>
                 <option value="middle">Middle</option>
