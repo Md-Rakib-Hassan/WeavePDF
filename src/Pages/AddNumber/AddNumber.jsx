a2b34bde9e3193bc4325ef7ae8febab278545e24
Maliha <maliha16tabassum@gmail.com>
2024-02-24 21:24:45 +0600

    color added

--- src/Pages/AddNumber/AddNumber.jsx
+++ src/Pages/AddNumber/AddNumber.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import useAxiosPublic from "../../hooks/useAxiosPublic";
-import { PDFDocument } from 'pdf-lib'
+import { PDFDocument,rgb } from 'pdf-lib'
 
 const AddNumber = () => {
-    const axiosPublic = useAxiosPublic();
     const [url, setUrl] = useState(null);
     const handleUpload = async (e) =>{
         e.preventDefault();
@@ -26,21 +24,51 @@ const AddNumber = () => {
             else if(position === 'right') bottomX = pageWidth - 70
             else bottomX = pageWidth/2
 
+            if (color === 'black'){
             page.drawText(text, {
                 x: bottomX,
                 y: bottomY,
                 size: 12,
-                color: color
-            })
+            })}
+
+            else if(color === 'blue'){
+                page.drawText(text, {
+                    x: bottomX,
+                    y: bottomY,
+                    size: 12,
+                    color: rgb(0,0,1)
+            })}
+
+            else if (color === 'red'){
+                page.drawText(text, {
+                    x: bottomX,
+                    y: bottomY,
+                    size: 12,
+                    color: rgb(0.95, 0.1, 0.1)
+                })
+            }
+
+            else{
+                page.drawText(text, {
+                    x: bottomX,
+                    y: bottomY,
+                    size: 12,
+                    color: rgb(0.04, 0.47, 0.41)
+                })
+            }
+            }
             const pdfBytes = await pdfDoc.save();
             const pdfBlob = new Blob([pdfBytes] , { type: 'application/pdf' })
             setUrl(URL.createObjectURL(pdfBlob))
         }
         
         
-    }
+    
     return (
         <div>
+            <br /><br />
+            <h1 className='text-3xl font-playfair text-center font-bold'>Add Page number to PDF Files here</h1><br />
+            <p className="text-center">Add page numbers into PDFs with ease. Choose your positions, dimensions, typography.</p>
             <form onSubmit={handleUpload} action="">
             <input type="file" accept="application/pdf" name="file" id="input" required /><br />
             <label htmlFor="position">Position for Page Number</label>
@@ -49,7 +77,13 @@ const AddNumber = () => {
                 <option value="right">Right</option>
                 <option value="middle">Middle</option>
             </select><br />
-            <input type="text" name="color" id="" placeholder="colorname or rgb or hex"/>
+            <select name="color" id="" required>
+                <option value="black">Black</option>
+                <option value="blue">Blue</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+            </select><br />
+            {/* <input type="text" name="font" id="" placeholder="add font"/> */}
             <button className="btn"><input type="submit" value="Add Number" /></button>
             </form>
             <div className="flex justify-center">
