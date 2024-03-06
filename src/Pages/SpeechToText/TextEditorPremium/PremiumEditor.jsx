bde96d13f47b0d2782dd1fd61f9b0cf416ee1ca8
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2024-03-06 22:28:30 +0600

    Created a editor for premium user

--- src/Pages/SpeechToText/TextEditorPremium/PremiumEditor.jsx
+++ src/Pages/SpeechToText/TextEditorPremium/PremiumEditor.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+import './TextEditorPremium.css'
 
 
 const PremiumEditor = () => {
+    const [value, setValue] = useState('');
+
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ font: [] }],
+            [{ size: [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image', 'video'],
+            ['clean']
+        ],
+    }
     return (
         <div>
+            <div className="container">
+                <div className="row">
+                    <div className="editor">
+                        <ReactQuill
+                            theme="snow"
+                            value={value}
+                            onChange={setValue}
+                            className='editor-input'
+                            modules={modules}
+                        />
+                    </div>
 
+
+                    <div className="preview" >
+                        <button>Download</button>
+                        <div dangerouslySetInnerHTML={{ __html: value }} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
