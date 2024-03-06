050c6a7e33131fa9b5500d9e7c686cded46e208f
Maliha <maliha16tabassum@gmail.com>
2024-03-07 01:26:06 +0600

    added landing image

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <div className='flex'>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
