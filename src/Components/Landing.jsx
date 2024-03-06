8098675c99bd67bd1d7be853f4e2ec581f1aeae9
Maliha <maliha16tabassum@gmail.com>
2024-03-07 02:01:10 +0600

    aligned landing section

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
@@ -7,8 +6,8 @@ const Landing = () => {
                 <div className="lg:w-1/3">
                 <h1 className="text-4xl lg:text-5xl font-bold">Powerful PDF<br />  Tools Made <span className="text-aqua_marine">Simple.</span></h1><br />
                 <p>Say goodbye to complicated software & hello to easy-to-use PDF tools. Edit, merge, convert, split, sign & sign your documents and many more in just a few clicks.</p>
-                <div className="py-6 flex flex-col md:flex-row gap-4">
-                    <a href="#tools"><button className="btn border-[#00c093] text-black font-bold w-full">Explore</button></a>
+                <div className="py-6">
+                    <a href="#tools"><button className="btn border-[#52ab98] text-black font-bold w-3/4 animate-pulse">Explore</button></a>
                 </div>
                 </div>
                 <div className="lg:w-2/3">
